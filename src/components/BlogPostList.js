8a24b2619c1b711d6ab67b2ba3137272e4df5ae3
Ernest Riccetto <ernest.riccetto@gmail.com>
2019-07-14 11:34:40 +0200

    litle maquetation in blogpost list, installed timeago library

--- src/components/BlogPostList.js
+++ src/components/BlogPostList.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import blogPostList from "../reducers/blogPostList";
+import timeago from 'timeago.js';
 
 class BlogPostList extends React.Component {
     render() {
         const {posts, isFetching} = this.props;
 
-        if (isFetching){
-            return(<div><i className="fa fa-spinner fa-spin"></i></div>);
+        if (isFetching) {
+            return (<div><i className="fa fa-spinner fa-spin"></i></div>);
         }
 
-        if (null === posts ||  0 === posts.length){
-            return(<div>No blogPost to see here</div>);
+        if (null === posts || 0 === posts.length) {
+            return (<div>No blogPost to see here</div>);
         }
 
-        return (
-            <div>
-                <ul>
-                    {posts && posts.map(post => <li key={post.id}>{post.title}</li>)}
-                </ul>
-            </div>
-        )
+        return (<div>
+            {posts && posts.map(post => (
+                <div className="card mb-3 mt-3 shadow-sm" key={post.id}>
+                    <div className="card-body">
+                        <h3>{post.title}</h3>
+                        <p className="card-text border-top">
+                            <small className="text-muted">{timeago().format(post.published)}</small>
+                        </p>
+                    </div>
+                </div>
+            ))}
+        </div>)
     }
 }
 
