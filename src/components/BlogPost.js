02db111837891849087ea4494d6f5588b1bc4e8d
Ernest Riccetto <ernest.riccetto@gmail.com>
2019-08-04 12:13:52 +0200

    maquetation and showing more info of individual blogpost

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import timeago from 'timeago.js';
 
 export class BlogPost extends React.Component {
     render() {
@@ -13,8 +14,15 @@ export class BlogPost extends React.Component {
         }
 
         return (
-            <div>
-                {post.title}
+            <div className=" card mb-3 mt-3 shadow-sm">
+                <div className="card-body">
+                    <h2>{post.title}</h2>
+                    <p className="card-terxt">{post.content}</p>
+                    <p className="card-text border-top">
+                        {timeago().format(post.published)} by&nbsp;
+                        {post.author.username}
+                    </p>
+                </div>
             </div>
         )
     }
