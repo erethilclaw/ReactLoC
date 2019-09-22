fa6e5824e1d7a1feca9f61e482434e2d8858fce1
Ernest Riccetto <ernest.riccetto@gmail.com>
2019-09-22 10:49:51 +0200

    BlogPost now render his comments, form library instaled

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -5,16 +5,29 @@ import Message from "./Message";
 export class CommentList extends React.Component {
     render() {
         const {commentList} = this.props;
+        console.log(commentList);
 
         if (null === commentList) {
             return (<Message message="This post has not comments yet"/>);
         }
 
         return (
-            <div className=" card mb-3 mt-3 shadow-sm">
-                <div className="card-body">
-                    Not done yet
-                </div>
+            <div className="card mb-3 mt-3 shadow-sm">
+                {commentList.map(comment => {
+                    return(
+                        <div className="card-body border-bottom" key={comment.id}>
+                            <p className="card-text mb-0">
+                                {comment.content}
+                            </p>
+                            <p className="card-text">
+                                <small className="text-muted">
+                                    {timeago().format(comment.created)} by&nbsp;
+                                    {comment.author.name} the {comment.author.username}
+                                </small>
+                            </p>
+                        </div>
+                    );
+                })}
             </div>
         )
     }
